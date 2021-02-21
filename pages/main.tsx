83add59236302974cfaade059c3d968a7c1e4e28
Reroet <wayanpuguhsudarma@gmail.com>
2021-02-21 11:08:13 +0800

    feat: get todo list

--- pages/main.tsx
+++ pages/main.tsx
@@ -1,13 +1,40 @@
-import { ChangeEvent, useCallback, useRef, useState } from 'react';
+import { ChangeEvent, useCallback, useEffect, useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../components/Button';
 import CardTodo from '../components/cardTodo';
 import TodoFormModal, { ModalOnSubmitProps } from '../components/TodoFormModal';
+import { getTodosLogic } from '../store/todo/todoLogic';
+import { AppState } from '../types/todo.model';
 
 type OrderByType = 'title' | 'author' | 'time';
 
 const Main = () => {
+  const dispatch = useDispatch();
   const modalRef = useRef<TodoFormModal>(null);
   const [orderBy, setOrderBy] = useState<OrderByType>('title');
+  const [name, setName] = useState('');
+  const todos = useSelector((state: AppState) => state.todo.todos);
+
+  useEffect(() => {
+    setName(localStorage.getItem('name'));
+  }, []);
+
+  useEffect(() => {
+    let unsubscribe: () => void | null = null;
+
+    // listen to todo
+    dispatch(
+      getTodosLogic((ref) => {
+        unsubscribe = ref;
+      })
+    );
+
+    return () => {
+      if (unsubscribe) {
+        unsubscribe();
+      }
+    };
+  }, []);
 
   const onChangeOrderBy = useCallback(
     (e: ChangeEvent<HTMLSelectElement>) => {
@@ -42,7 +69,7 @@ const Main = () => {
   return (
     <div className="bg-gray-200 flex w-screen h-screen">
       <div className="container mx-auto px-4 md:px-0">
-        <h1 className="text-4xl mt-10">{'Hi, Reroet'}</h1>
+        <h1 className="text-4xl mt-10">{`Hi, ${name}`}</h1>
 
         <div className="grid grid-cols-1 md:grid-cols-2 mt-10 gap-4">
           <div>
@@ -72,19 +99,17 @@ const Main = () => {
         </div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mt-10">
-          {[0, 1, 2, 3, 4, 5].map((item) => {
+          {todos.map((item) => {
             return (
               <CardTodo
-                key={item}
-                id={'${item}'}
-                image={
-                  'https://assets.pikiran-rakyat.com/crop/0x0:0x0/x/photo/2021/02/15/1844222189.jpg'
-                }
-                title={'This is long long text hahah ah ha ahha'}
-                description={'Lorem ipsum dolor sit amet, consectetur adipiscing elit'}
-                createdAt={'02 March 2020, 02:00 PM'}
-                author={'Reroet'}
-                isDone={true}
+                key={item.id}
+                id={item.id}
+                image={item.image}
+                title={item.title}
+                description={item.description}
+                createdAt={new Date(item.created_date.seconds).toString()}
+                author={item.user_name}
+                isDone={item.done}
                 onMarkDoneClick={onMarkDoneClick}
                 onEditClick={onUpdateTodoClick}
                 onDeleteClick={onDeleteClick}
